6e1f98e3347841ef6c219d7bb01a3333788ee671
yurivolkov21 <yurivolkov1210@gmail.com>
2026-02-26 21:13:36 +0700

    feat: define UserRole and UserDoc types for user management

---
+++ src/modules/user/user.model.ts
@@ -0,0 +1,12 @@
+export type UserRole = "customer" | "admin";
+
+export type UserDoc = {
+    email: string;
+    passwordHash: string;
+
+    role: UserRole;
+
+    createdAt: Date;
+    updatedAt: Date;
+}
+
