2fdc05db6affd5f136d33127b53f005670efddb6
yurivolkov21 <yurivolkov1210@gmail.com>
2026-03-04 09:41:44 +0700

    feat: update user and auth routes in app configuration

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import { userRoutes } from "./modules/user/user.route.js";
+import { authRoutes } from "./modules/auth/auth.route.js";
 import { errorMiddleware } from "./middleware/error.middleware.js";
 
 export function createApp() {
@@ -9,7 +10,8 @@ export function createApp() {
     app.use(express.json());
     app.use(cookieParser());
 
-    app.use("/api/user", userRoutes);
+    app.use("/api/users", userRoutes);
+    app.use("/api/auth", authRoutes);
 
     app.use(errorMiddleware);
 
